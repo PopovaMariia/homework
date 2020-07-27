6ebc4973818a49aafe6aae1ed5c00548399c428d
Mariia <popova.mariia21@gmail.com>
2020-07-27 19:39:38 +0300

    Added fixed homework_8

--- homework_8/Mariia_Popova_Homework_8_part_2.py
+++ homework_8/Mariia_Popova_Homework_8_part_2.py
@@ -1,37 +1,42 @@
 # 3 Угадать число
 import random
-import sys
+from typing import Set
 
 random_number = random.randint(1, 50)
-user_number = input('Please, write any number from 1 to 50 ')
+user_number = 0
+attempt = 1
 
 
 def validation(user_number):
     if not isinstance(user_number, (str, int)):
-        return 'Please use number'
+        return 'Fail'
     if isinstance(user_number, str) and user_number.isdigit():
         user_number = int(user_number)
-    else:
-        return 'Please use number'
-    return
+        return
+    return 'Fail'
 
 
 def compare():
     global user_number
-    validation(user_number)
-    for attempt in range(1, 10, 1):
-        if user_number == random_number:
-            return attempt
+    global attempt
+    for attempt in range(10):
+        if int(user_number) == random_number:
+            return 'You won'
         user_number = input("Number: ")
-    return sys.exit()
+        if validation(user_number) == 'Fail':
+            return 'You need to use only numbers'
+    return 'Game over'
 
 
-compare()
+print(f'Please, write any number from 1 to 50.')
+print(compare())
 
 # 4 Сохраним последний успешный запуск
 import json
 import datetime
 
 data = {'Number': user_number, 'Attemp': attempt, 'Date': str(datetime.datetime.today())}
-with open('result.json', 'w') as file:
-    json.dump(data, file, indent=4)
+if validation(user_number) != 'Fail':
+    if int(user_number) == random_number:
+        with open('result.json', 'w') as file:
+            json.dump(data, file, indent=4)
